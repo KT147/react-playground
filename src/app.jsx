9e5c88b1b7891d82529d9aa7e5cdea57b61f820f
KT147 <kevin.tammm@gmail.com>
2025-12-09 14:21:24 +0200

    .

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,13 @@
-import { useEffect, useState } from "react";
+import Calculator from "./components/Calculator.jsx";
+import Header from "./components/Header";
+
 
 function App() {
-  const [text, setText] = useState("Good morning");
-  const [textColor, setTextColor] = useState({ color: "red" });
-
-  useEffect(() => {
-    const d = new Date();
-    let hour = d.getHours();
-
-    if (hour >= 0 && hour < 12) {
-      setText("Good night");
-      setTextColor({ color: "blue" });
-    } else if (hour >= 12 && hour < 18) {
-      setText("Good evening");
-      setTextColor({ color: "green" });
-    } else {
-      setText("Good morning");
-    }
-  }, []);
 
   return (
     <div>
-      <h1 style={textColor}>{text}</h1>
+      <Header/>
+      <Calculator/>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+function Header() {
+  const [text, setText] = useState("Good morning");
+  const [textColor, setTextColor] = useState({ color: "red" });
+
+  useEffect(() => {
+    const d = new Date();
+    let hour = d.getHours();
+
+    if (hour >= 0 && hour < 12) {
+      setText("Good night");
+      setTextColor({ color: "blue" });
+    } else if (hour >= 12 && hour < 18) {
+      setText("Good evening");
+      setTextColor({ color: "green" });
+    } else {
+      setText("Good morning");
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1 style={textColor}>{text}</h1>
+    </div>
+  );
+}
+
+export default Header;
